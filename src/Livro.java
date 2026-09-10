1774a489fc9ec003cac73b2408e6971071317271
Gabriel Machado Freitas <gabrielmachadofreitass@gmail.comm>
2026-09-09 22:00:30 -0300

    commit inicial

---
+++ src/Livro.java
@@ -0,0 +1,8 @@
+public class Livro {
+    String titulo;
+    boolean disponibilidade;
+
+    public void cadastrarLivro() {}
+
+    public void listarLivro() {}
+}
---
+++ src/Usuarios.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Usuarios {
+    String nome;
+    int idade;
+    ArrayList<Livro> livros;
+
+    public void cadastrarCliente() {}
+
+    public void listarUsuarios() {}
+
+    public void alugarLivros() {}
+
+    public void realizarDevolucao() {}
+
+
+}
